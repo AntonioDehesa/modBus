0cf7f9e2bff29f63d6721c801cbe758dc91a1e24
Heriberto Antonio Dehesa Ortiz <hdehesaortiz@gmail.com>
2020-11-13 01:02:52 -0600

    Agregar archivos de proyecto.

---
+++ modBus1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace modBus1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toggleButton1 = new ToggleButton_Demo.ToggleButton();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 19200;
+            this.serialPort1.DtrEnable = true;
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.RtsEnable = true;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Consulta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(435, 54);
+            this.progressBar1.Maximum = 1023;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 38);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // toggleButton1
+            // 
+            this.toggleButton1.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(161)))), ((int)(((byte)(226)))));
+            this.toggleButton1.ActiveText = "ON";
+            this.toggleButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.toggleButton1.InActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.toggleButton1.InActiveText = "OFF";
+            this.toggleButton1.Location = new System.Drawing.Point(94, 54);
+            this.toggleButton1.MaximumSize = new System.Drawing.Size(119, 32);
+            this.toggleButton1.MinimumSize = new System.Drawing.Size(75, 23);
+            this.toggleButton1.Name = "toggleButton1";
+            this.toggleButton1.Size = new System.Drawing.Size(75, 23);
+            this.toggleButton1.SliderColor = System.Drawing.Color.Black;
+            this.toggleButton1.SlidingAngle = 8;
+            this.toggleButton1.TabIndex = 0;
+            this.toggleButton1.Text = "toggleButton1";
+            this.toggleButton1.TextColor = System.Drawing.Color.White;
+            this.toggleButton1.ToggleState = ToggleButton_Demo.ToggleButton.ToggleButtonState.OFF;
+            this.toggleButton1.ToggleStyle = ToggleButton_Demo.ToggleButton.ToggleButtonStyle.Android;
+            this.toggleButton1.ButtonStateChanged += new ToggleButton_Demo.ToggleButton.ToggleButtonStateChanged(this.toggleButton1_ButtonStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 288);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toggleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToggleButton_Demo.ToggleButton toggleButton1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ modBus1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modBus1
+{
+    public partial class Form1 : Form
+    {
+        static byte SLAVE01 = 1;
+        static byte WRITE_COIL = 5;
+        static byte READ_COIL = 4;
+        static byte COIL_REGISTER = 0;
+        static byte COIL_ON = 0xFF;
+        static byte COIL_OFF = 0x00;
+        bool slaveOneStatus = false;
+        //public byte[] arr1 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+                label1.Text = "Port opened";
+            }
+            else
+            {
+                label1.Text = "Port busy";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toggleButton1_ButtonStateChanged(object sender, ToggleButton_Demo.ToggleButton.ToggleButtonStateEventArgs e)
+        {
+            byte[] arr1 = { SLAVE01, WRITE_COIL, COIL_REGISTER, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            if (slaveOneStatus == false)
+            {
+                slaveOneStatus = true;
+                arr1[4] = COIL_ON;
+            }
+            else
+            {
+                slaveOneStatus = false;
+                arr1[4] = COIL_OFF;
+            }
+            SendString(arr1);
+        }
+
+        public void SendString(byte[] arr)
+        {
+            uint ModbusCRC = CalcCRC(arr);
+            arr[7] = Convert.ToByte(ModbusCRC & 0xFF);
+            arr[6] = Convert.ToByte((ModbusCRC >> 8) & 0xFF);
+            for (int n = 0; n <= 7; n++)
+            {
+                byte[] ch = new byte[1];
+                ch[0] = arr[n];
+                serialPort1.Write(ch, 0, 1);
+            }
+        }
+
+        public uint CalcCRC(byte[] ch)
+        {
+            uint crc16 = 0xFFFF;
+            uint temp;
+            uint flag;
+            for (int i = 0; i < 6; i++)
+            {
+                temp = (uint)ch[i]; // temp has the first byte 
+                temp &= 0x00ff; // mask the MSB 
+                crc16 = crc16 ^ temp; //crc16 XOR with temp 
+                for (uint c = 0; c < 8; c++)
+                {
+                    flag = crc16 & 0x01; // LSBit di crc16 is mantained
+                    crc16 = crc16 >> 1; // Lsbit di crc16 is lost 
+                    if (flag != 0)
+                        crc16 = crc16 ^ 0x0a001; // crc16 XOR with 0x0a001 
+                }
+            }
+            crc16 = (crc16 >> 8) | (crc16 << 8); // LSB is exchanged with MSB
+            crc16 = crc16 & 0xFFFF;
+            return (crc16);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //byte[] arr1 = { 0x01, 0x04, 0x00, 0x00, 0x00, 0x01, 0x31, 0xCA };
+            byte[] arr1 = { SLAVE01, READ_COIL, COIL_REGISTER, 0x00, 0x00, 0x01, 0x00, 0x00 };
+            SendString(arr1);
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            byte[] buffer = { 0x00, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 };
+            serialPort1.Read(buffer, 0, 7);
+
+            if(buffer[1] == READ_COIL && buffer[2] == 2)//Read coil and two arguments
+            {
+                int valorUpper = Convert.ToInt32(buffer[3]) << 8;
+                int valorLower = Convert.ToInt32(buffer[4]);
+                int valor = (valorUpper | valorLower);
+                /*Console.WriteLine("Valor del buffer = {0},{1},{2},{3},{4},{5},{6},{7}", buffer[0]
+                    , buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7]);
+                Console.WriteLine("Upper = {0}", valorUpper);
+                Console.WriteLine("Lower = {0}", valorLower);
+                Console.WriteLine("Valor leido = {0}",valor);*/
+                if(progressBar1.InvokeRequired)
+                {
+                    _ = progressBar1.Invoke(new MethodInvoker(delegate
+                      {
+                          progressBar1.Value = valor;
+                      }));
+                }
+            }
+            //progressBar1.Value = valor;
+        }
+    }
+}
